8bc4484a0fa39b69c7f9deda75a7b04f4fc1d4a5
MaxSchalk <ge96gez@mytum.de>
2021-12-13 13:57:23 +0100

    prettyprint

--- src/task13.py
+++ src/task13.py
@@ -107,6 +107,10 @@ def fold(dot_coordinates, fold_instruction):
         dot_coordinate[axis] = new_value
 
 
+EMPTY = ' '
+FILLED = '\u25A0'
+
+
 def _solve_part_two(data):
     dot_coordinates, fold_instructions = data
 
@@ -118,11 +122,64 @@ def _solve_part_two(data):
     max_x = max(map(lambda t: t[0], final_coords))
     max_y = max(map(lambda t: t[1], final_coords))
 
-    result = [list(' ' * (max_x + 1)) for _ in range(max_y + 1)]
+    result = [list(EMPTY * (max_x + 1)) for _ in range(max_y + 1)]
 
     for y, row in enumerate(result):
         for x, _ in enumerate(row):
             if (x, y) in final_coords:
-                result[y][x] = '#'
+                result[y][x] = FILLED
 
-    print("\n".join(map(lambda r: str.join("", r), result)))
+    pretty_print(result)
+
+
+def pretty_print(data):
+
+    transposed = list(zip(*data))
+
+    spaced = []
+
+    for row in transposed:
+        if all(map(lambda c: c == EMPTY, row)):
+            for _ in range(3):
+                spaced.append(row)
+
+        spaced.append(row)
+
+    data = list(map(list, zip(*spaced)))
+
+    for y, row in enumerate(data):
+        for x, value in enumerate(row):
+            if value == EMPTY:
+                continue
+
+            top = y > 0 and data[y-1][x] != EMPTY
+            bottom = y < len(data) - 1 and data[y+1][x] != EMPTY
+
+            left = x > 0 and data[y][x-1] != EMPTY
+            right = x < len(data[0]) - 1 and data[y][x+1] != EMPTY
+
+            top_left = y > 0 and x > 0 and data[y-1][x-1] != EMPTY
+            bottom_left = y < len(data) - 1 and x > 0 and data[y+1][x-1] != EMPTY
+            bottom_right = y < len(data) - 1 and x < len(data[0]) - 1 and data[y+1][x+1] != EMPTY
+            top_right = y > 0 and x < len(data[0]) - 1 and data[y-1][x+1] != EMPTY
+
+            if top and bottom:
+                row[x] = '|'
+            elif left and right:
+                row[x] = '-'
+            elif top or bottom:
+                row[x] = '|'
+            elif left or right:
+                row[x] = '-'
+            elif top_left or bottom_right:
+                row[x] = '\\'
+            elif bottom_left or top_right:
+                row[x] = '/'
+
+    print("\u001b[40;1m", end="")
+    print("\u001b[33;1m", end="")
+    print("\u001b[1m", end="")
+
+    print("\n".join(map(lambda r: f"{str.join('', r)}", data)))
+
+    print("\u001b[0m")
