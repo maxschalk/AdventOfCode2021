39f2da7b3ef1c7b825dcf5f05f34f509849c1619
Maximilian Schalk <maximilian.schalk@audi.de>
2021-12-14 15:25:57 +0100

    solve task14

--- src/task14.py
+++ src/task14.py
@@ -1,3 +1,4 @@
+import collections
 import re
 
 from pathlib import Path
@@ -38,16 +39,18 @@ def read_input(test: bool):
 
 
 def parse_input(data, test):
+    template, sep_line, *insertions = data
+
     out = []
 
-    pattern = re.compile(r"(?s)(.+)")
+    pattern = re.compile(r"([A-Z]+) -> ([A-Z]+)")
 
-    for line in data:
-        result = pattern.match(line).groups()
+    for line in insertions:
+        start, end = pattern.match(line).groups()
 
-        out.append(result)
+        out.append((start, end))
 
-    return out
+    return template, out
 
 
 def solve_test(data):
@@ -57,17 +60,6 @@ def solve_test(data):
         print('-' * 15)
 
         print(f"Part 2 - {_solve_part_two(data)}")
-
-
-def solve_test_individual_lines(data):
-    with suppress(NotImplementedError):
-        for line in data:
-            print(f"Part 1: {line}: {_solve_part_one(line)}")
-
-        print('-' * 30)
-
-        for line in data:
-            print(f"Part 2: {line}: {_solve_part_two(line)}")
 
 
 def solve(data):
@@ -87,8 +79,65 @@ def solve_part_two(data):
 # SOLUTION
 
 def _solve_part_one(data):
-    print(data)
+    return _solve(data, 10)
 
 
 def _solve_part_two(data):
-    raise NotImplementedError
+    return _solve(data, 40)
+
+
+def _solve(data, steps):
+    template, insertions = data
+
+    insertion_map = {tuple(start): end for start, end in insertions}
+
+    pair_counts = get_pair_counter(template)
+
+    for _ in range(steps):
+        pair_counts = insertion_step(pair_counts, insertion_map)
+
+    counts = get_single_counts(pair_counts, template, as_sorted_list=True)
+
+    min_elem = counts[0]
+    max_elem = counts[-1]
+
+    return max_elem[1] - min_elem[1]
+
+
+def get_pair_counter(polymer):
+    elements = collections.defaultdict(int)
+
+    for index in range(len(polymer) - 1):
+        char1 = polymer[index]
+        char2 = polymer[index + 1]
+
+        elements[(char1, char2)] += 1
+
+    return elements
+
+
+def insertion_step(pair_counts, insertion_map):
+    new_pair_counts = collections.defaultdict(int)
+
+    for key, value in pair_counts.items():
+        char1, char2 = key
+        insertion_char = insertion_map[key]
+
+        new_pair_counts[(char1, insertion_char)] += value
+        new_pair_counts[(insertion_char, char2)] += value
+
+    return new_pair_counts
+
+
+def get_single_counts(pair_counts, original_polymer, as_sorted_list=False):
+    counts = collections.defaultdict(int)
+
+    for (char1, char2), value in pair_counts.items():
+        counts[char1] += value
+
+    counts[original_polymer[-1]] += 1
+
+    if as_sorted_list:
+        return sorted(((char, count) for char, count in counts.items()), key=lambda t: t[1])
+
+    return counts
